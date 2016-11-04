52674e9d68b8bdb23ee9b220284e46d7b2246a56
eacdy0000@126.com <eacdy0000@126.com>
2016-11-04 16:46:31 +0800

    将应用注册到了eureka上。

--- microservice-consumer-movie/src/main/java/com/itmuch/cloud/study/ConsumerMovieApplication.java
+++ microservice-consumer-movie/src/main/java/com/itmuch/cloud/study/ConsumerMovieApplication.java
@@ -2,9 +2,11 @@ package com.itmuch.cloud.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConsumerMovieApplication {
   @Bean
