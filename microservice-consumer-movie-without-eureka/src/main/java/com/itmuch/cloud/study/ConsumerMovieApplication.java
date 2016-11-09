5f549adabc973152b53299b39ae2e1e44dc77988
eacdy0000@126.com <eacdy0000@126.com>
2016-11-09 17:49:23 +0800

    ribbon without eureka

---
+++ microservice-consumer-movie-without-eureka/src/main/java/com/itmuch/cloud/study/ConsumerMovieApplication.java
@@ -0,0 +1,20 @@
+package com.itmuch.cloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ConsumerMovieApplication {
+  @Bean
+  @LoadBalanced
+  public RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConsumerMovieApplication.class, args);
+  }
+}
