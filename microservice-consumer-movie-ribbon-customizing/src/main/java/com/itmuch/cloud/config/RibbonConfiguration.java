a781d21c9ccbd675a533ada4db23f017630f06a6
eacdy0000@126.com <eacdy0000@126.com>
2016-11-09 14:28:14 +0800

    添加注释。

--- microservice-consumer-movie-ribbon-customizing/src/main/java/com/itmuch/cloud/config/RibbonConfiguration.java
+++ microservice-consumer-movie-ribbon-customizing/src/main/java/com/itmuch/cloud/config/RibbonConfiguration.java
@@ -7,14 +7,15 @@ import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.RandomRule;
 
 /**
- * 该类为Ribbon的配置类，注意：该类不能与@ComponentScan扫描
- * @author Administrator
- *
+ * 该类为Ribbon的配置类
+ * 注意：该不应该在主应用程序上下文的@ComponentScan中。
+ * @author 周立
  */
 @Configuration
 public class RibbonConfiguration {
   @Bean
   public IRule ribbonRule() {
+    // 负载均衡规则，改为随机
     return new RandomRule();
   }
 }
--- microservice-consumer-movie-ribbon-customizing/src/main/java/com/itmuch/cloud/study/config/TestConfiguration.java
+++ microservice-consumer-movie-ribbon-customizing/src/main/java/com/itmuch/cloud/study/config/TestConfiguration.java
@@ -5,6 +5,13 @@ import org.springframework.context.annotation.Configuration;
 
 import com.itmuch.cloud.config.RibbonConfiguration;
 
+/**
+ * 使用RibbonClient，为特定name的Ribbon Client自定义配置.
+ * 使用@RibbonClient的configuration属性，指定Ribbon的配置类.
+ * 可参考的示例：
+ * http://spring.io/guides/gs/client-side-load-balancing/
+ * @author 周立
+ */
 @Configuration
 @RibbonClient(name = "microservice-provider-user", configuration = RibbonConfiguration.class)
 public class TestConfiguration {
