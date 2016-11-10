ccf92167525d5ffc6405f12ff68cc5ec223d2761
eacdy0000@126.com <eacdy0000@126.com>
2016-11-10 14:14:50 +0800

    feign

---
+++ microservice-consumer-movie-feign/src/main/java/com/itmuch/cloud/study/user/controller/MovieController.java
@@ -0,0 +1,20 @@
+package com.itmuch.cloud.study.user.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.itmuch.cloud.study.user.entity.User;
+import com.itmuch.cloud.study.user.feign.UserFeignClient;
+
+@RestController
+public class MovieController {
+  @Autowired
+  private UserFeignClient userFeignClient;
+
+  @GetMapping("/user/{id}")
+  public User findById(@PathVariable Long id) {
+    return this.userFeignClient.findById(id);
+  }
+}
---
+++ microservice-consumer-movie-feign/src/main/java/com/itmuch/cloud/study/user/feign/UserFeignClient.java
@@ -0,0 +1,14 @@
+package com.itmuch.cloud.study.user.feign;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.itmuch.cloud.study.user.entity.User;
+
+@FeignClient(name = "microservice-provider-user")
+public interface UserFeignClient {
+  @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+  public User findById(@PathVariable("id") Long id);
+}
