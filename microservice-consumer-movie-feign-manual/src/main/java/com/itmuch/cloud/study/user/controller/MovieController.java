c1bc3c66d1bc03099d0d559a1dca700adcb346a2
eacdy0000@126.com <eacdy0000@126.com>
2016-11-15 11:57:45 +0800

    feign 手动创建

--- microservice-consumer-movie-feign-manual/src/main/java/com/itmuch/cloud/study/user/controller/MovieController.java
+++ microservice-consumer-movie-feign-manual/src/main/java/com/itmuch/cloud/study/user/controller/MovieController.java
@@ -2,8 +2,6 @@ package com.itmuch.cloud.study.user.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.feign.FeignClientsConfiguration;
-import org.springframework.cloud.netflix.feign.support.ResponseEntityDecoder;
-import org.springframework.cloud.netflix.feign.support.SpringEncoder;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,8 +11,11 @@ import com.itmuch.cloud.study.user.entity.User;
 import com.itmuch.cloud.study.user.feign.UserFeignClient;
 
 import feign.Client;
+import feign.Contract;
 import feign.Feign;
 import feign.auth.BasicAuthRequestInterceptor;
+import feign.codec.Decoder;
+import feign.codec.Encoder;
 
 @Import(FeignClientsConfiguration.class)
 @RestController
@@ -24,10 +25,11 @@ public class MovieController {
   private UserFeignClient adminUserFeignClient;
 
   @Autowired
-  public MovieController(ResponseEntityDecoder decoder, SpringEncoder encoder, Client client) {
-    this.userUserFeignClient = Feign.builder().client(client).encoder(encoder).decoder(decoder)
+  public MovieController(Decoder decoder, Encoder encoder, Client client, Contract contract) {
+    // 这边的decoder、encoder、client、contract，可以debug看看是什么实例。
+    this.userUserFeignClient = Feign.builder().client(client).encoder(encoder).decoder(decoder).contract(contract)
         .requestInterceptor(new BasicAuthRequestInterceptor("user", "password1")).target(UserFeignClient.class, "http://microservice-provider-user/");
-    this.adminUserFeignClient = Feign.builder().client(client).encoder(encoder).decoder(decoder)
+    this.adminUserFeignClient = Feign.builder().client(client).encoder(encoder).decoder(decoder).contract(contract)
         .requestInterceptor(new BasicAuthRequestInterceptor("admin", "password2"))
         .target(UserFeignClient.class, "http://microservice-provider-user/");
   }
