9a4619682d13c30fb50391a865bdb34ab21feffb
eacdy0000@126.com <eacdy0000@126.com>
2016-11-17 10:56:41 +0800

    Hystrix Dashboard

---
+++ microservice-hystrix-dashboard/src/main/java/com/itmuch/cloud/study/HystrixDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.itmuch.cloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(HystrixDashboardApplication.class, args);
+  }
+}
