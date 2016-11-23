4c05958350359b1651042f4cfd30fdab3db222e1
eacdy0000@126.com <eacdy0000@126.com>
2016-11-23 18:43:18 +0800

    zuul filter

---
+++ microservice-gateway-zuul-filter/src/main/java/com/itmuch/cloud/study/ZuulApplication.java
@@ -0,0 +1,21 @@
+package com.itmuch.cloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.itmuch.cloud.study.filters.pre.SimpleFilter;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ZuulApplication.class, args);
+  }
+
+  @Bean
+  public SimpleFilter simpleFilter() {
+    return new SimpleFilter();
+  }
+}
---
+++ microservice-gateway-zuul-filter/src/main/java/com/itmuch/cloud/study/filters/pre/SimpleFilter.java
@@ -0,0 +1,36 @@
+package com.itmuch.cloud.study.filters.pre;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+public class SimpleFilter extends ZuulFilter {
+  private static final Logger LOGGER = LoggerFactory.getLogger(SimpleFilter.class);
+
+  @Override
+  public String filterType() {
+    return "pre";
+  }
+
+  @Override
+  public int filterOrder() {
+    return 1;
+  }
+
+  @Override
+  public boolean shouldFilter() {
+    return true;
+  }
+
+  @Override
+  public Object run() {
+    RequestContext ctx = RequestContext.getCurrentContext();
+    HttpServletRequest request = ctx.getRequest();
+    SimpleFilter.LOGGER.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+    return null;
+  }
+}
