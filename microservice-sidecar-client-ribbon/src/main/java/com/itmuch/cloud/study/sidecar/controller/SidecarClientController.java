133bea5cb47e443c07cb7bbc9180e57a268a8082
eacdy0000@126.com <eacdy0000@126.com>
2016-11-27 19:44:27 +0800

    通过sidecar 请求异构系统demo

---
+++ microservice-sidecar-client-ribbon/src/main/java/com/itmuch/cloud/study/sidecar/controller/SidecarClientController.java
@@ -0,0 +1,18 @@
+package com.itmuch.cloud.study.sidecar.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class SidecarClientController {
+  @Autowired
+  private RestTemplate restTemplate;
+
+  @GetMapping("/test")
+  public String findById() {
+    // 将会请求到：http://localhost:8060/
+    return this.restTemplate.getForObject("http://microservice-sidecar-node-service/", String.class);
+  }
+}
