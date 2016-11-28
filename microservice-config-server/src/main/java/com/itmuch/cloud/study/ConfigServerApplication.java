42a106ba4b025ae4869557c95857804da693cffb
eacdy0000@126.com <eacdy0000@126.com>
2016-11-28 18:09:21 +0800

    config server

---
+++ microservice-config-server/src/main/java/com/itmuch/cloud/study/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.itmuch.cloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigServerApplication.class, args);
+  }
+}
