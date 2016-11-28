b10f3cc545e520a0459729d13a57a6cc666e14fe
eacdy0000@126.com <eacdy0000@126.com>
2016-11-28 21:31:40 +0800

    config client

---
+++ microservice-config-client/src/main/java/com/itmuch/cloud/study/controller/ConfigClientController.java
@@ -0,0 +1,22 @@
+package com.itmuch.cloud.study.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 这边的@RefreshScope注解不能少
+ * @author 周立
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+  @Value("${profile}")
+  private String profile;
+
+  @GetMapping("/profile")
+  public String hello() {
+    return this.profile;
+  }
+}
