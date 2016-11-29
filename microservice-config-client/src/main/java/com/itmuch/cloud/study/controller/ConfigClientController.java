8c8bd90836f9d1fd8420f2527a84635c551582ee
eacdy0000@126.com <eacdy0000@126.com>
2016-11-29 11:52:37 +0800

    cofig client

--- microservice-config-client/src/main/java/com/itmuch/cloud/study/controller/ConfigClientController.java
+++ microservice-config-client/src/main/java/com/itmuch/cloud/study/controller/ConfigClientController.java
@@ -1,16 +1,10 @@
 package com.itmuch.cloud.study.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * 这边的@RefreshScope注解不能少
- * @author 周立
- */
 @RestController
-@RefreshScope
 public class ConfigClientController {
   @Value("${profile}")
   private String profile;
