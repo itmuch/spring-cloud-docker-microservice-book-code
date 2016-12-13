dbf1791b114676c0ccc201038682f9940bc2ec17
eacdy0000@126.com <eacdy0000@126.com>
2016-12-13 14:23:46 +0800

    docker-compose

---
+++ docker/microservice-provider-user/src/main/java/com/itmuch/cloud/study/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.itmuch.cloud.study.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.itmuch.cloud.study.entity.User;
+import com.itmuch.cloud.study.repository.UserRepository;
+
+@RestController
+public class UserController {
+  @Autowired
+  private UserRepository userRepository;
+
+  @GetMapping("/{id}")
+  public User findById(@PathVariable Long id) {
+    User findOne = this.userRepository.findOne(id);
+    return findOne;
+  }
+}
