bb740f11a58eaf7d134975c7d7e21fdfb98817a9
eacdy0000@126.com <eacdy0000@126.com>
2016-12-15 14:45:48 +0800

    host网络模式，docker-compose

---
+++ docker-host/microservice-hystrix-turbine/src/main/java/com/itmuch/cloud/study/TurbineApplication.java
@@ -0,0 +1,13 @@
+package com.itmuch.cloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableTurbine
+public class TurbineApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(TurbineApplication.class, args);
+  }
+}
