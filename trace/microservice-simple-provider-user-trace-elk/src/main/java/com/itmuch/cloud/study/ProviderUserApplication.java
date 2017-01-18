448b88ff375536bc31fc981160c506132af1d763
eacdy0000@126.com <eacdy0000@126.com>
2017-01-18 12:51:14 +0800

    sleuth整合ELK

---
+++ trace/microservice-simple-provider-user-trace-elk/src/main/java/com/itmuch/cloud/study/ProviderUserApplication.java
@@ -0,0 +1,11 @@
+package com.itmuch.cloud.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderUserApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ProviderUserApplication.class, args);
+  }
+}
