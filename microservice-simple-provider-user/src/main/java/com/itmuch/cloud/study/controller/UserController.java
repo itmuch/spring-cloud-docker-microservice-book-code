a0a20786b83518402c00b94b7f7ee88f2c4c6f15
itmuch <itmuch0000@126.com>
2018-08-22 01:49:54 +0800

    2.0中 actuator不支持读取data.sql初始化数据。

--- microservice-simple-provider-user/src/main/java/com/itmuch/cloud/study/controller/UserController.java
+++ microservice-simple-provider-user/src/main/java/com/itmuch/cloud/study/controller/UserController.java
@@ -8,14 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.itmuch.cloud.study.entity.User;
 import com.itmuch.cloud.study.repository.UserRepository;
 
+import java.util.Optional;
+
 @RestController
 public class UserController {
   @Autowired
   private UserRepository userRepository;
 
   @GetMapping("/{id}")
-  public User findById(@PathVariable Long id) {
-    User findOne = this.userRepository.findOne(id);
-    return findOne;
+  public Optional<User> findById(@PathVariable Long id) {
+    return this.userRepository.findById(id);
   }
 }
