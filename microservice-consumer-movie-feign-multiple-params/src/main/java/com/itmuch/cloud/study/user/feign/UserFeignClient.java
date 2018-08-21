9e2d2c98fea282d6c456ddf5a12c19008a437679
itmuch <itmuch0000@126.com>
2018-08-22 01:47:39 +0800

    升级版本到boot 2.0.4 & cloud finchley sr1

--- microservice-consumer-movie-feign-multiple-params/src/main/java/com/itmuch/cloud/study/user/feign/UserFeignClient.java
+++ microservice-consumer-movie-feign-multiple-params/src/main/java/com/itmuch/cloud/study/user/feign/UserFeignClient.java
@@ -1,7 +1,7 @@
 package com.itmuch.cloud.study.user.feign;
 
 import com.itmuch.cloud.study.user.entity.User;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
